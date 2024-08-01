856faa8cf9a4fffa0f6184e11aa4966a889b0b0d
YunSukHyun <czcz9207@gmail.com>
2024-08-01 11:35:17 +0900

    init

---
+++ src/main/java/com/example/board/EcoTradeApplication.java
@@ -0,0 +1,11 @@
+package com.example.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoTradeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EcoTradeApplication.class, args);
+	}
+}
