eb83fb5a49a7d2695f4fe4d9aa2110d1482af6ca
YunSukHyun <czcz9207@gmail.com>
2024-08-06 10:40:04 +0900

    member, product 컨트롤러 제외 밑 작업

---
+++ src/main/java/com/example/board/model/member/Member.java
@@ -0,0 +1,31 @@
+package com.example.board.model.member;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter @ToString
+public class Member {
+	@Id
+	@Column(length=20)
+	private String member_id;
+	
+	@Column(length=20, nullable = false)
+	private String password;
+	
+	@Column(length=50, nullable = false)
+	private String name;
+	
+	private LocalDate birth;
+	
+	@Column(length=100)
+	private String email;
+	
+	private Long eco_point;
+}
