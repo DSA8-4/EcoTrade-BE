23708daf16f0286302ca34a2973cc2a5ca78b5cf
Suna Choi <su00021zu@gmail.com>
2024-09-09 11:07:19 +0900

    merge

--- src/main/java/com/example/board/repository/ProductLikeRepository.java
+++ src/main/java/com/example/board/repository/ProductLikeRepository.java
@@ -3,7 +3,6 @@ package com.example.board.repository;
 import com.example.board.model.member.Member;
 import com.example.board.model.product.Product;
 import com.example.board.model.product.ProductLike;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductLikeRepository extends JpaRepository<ProductLike, Long> {
