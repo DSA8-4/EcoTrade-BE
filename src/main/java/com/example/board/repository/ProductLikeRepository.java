874f47ff92f56c4ec6fd10383feb89b48e92f907
hyunjun Cho <chj7028@gmail.com>
2024-09-09 12:26:54 +0900

    상품찜하기 기능

--- src/main/java/com/example/board/repository/ProductLikeRepository.java
+++ src/main/java/com/example/board/repository/ProductLikeRepository.java
@@ -3,8 +3,12 @@ package com.example.board.repository;
 import com.example.board.model.member.Member;
 import com.example.board.model.product.Product;
 import com.example.board.model.product.ProductLike;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductLikeRepository extends JpaRepository<ProductLike, Long> {
     boolean existsByProductAndMember(Product product, Member member);
+    Optional<ProductLike> findByProductAndMember(Product product, Member member);
 }
