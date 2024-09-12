ee2c420527f4816fe2e2254e372ed0e6771be048
Suna Choi <su00021zu@gmail.com>
2024-09-12 10:49:30 +0900

    sales fix, resister fix

--- src/main/java/com/example/board/repository/MemberRepository.java
+++ src/main/java/com/example/board/repository/MemberRepository.java
@@ -3,10 +3,20 @@ package com.example.board.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.board.model.member.Member;
 
-public interface MemberRepository extends JpaRepository<Member, String>{
-	 Optional<Member> findByEmail(String email);
+public interface MemberRepository extends JpaRepository<Member, String> {
+	Optional<Member> findByEmail(String email);
+
+	 @Query("SELECT CASE WHEN COUNT(m) > 0 THEN TRUE ELSE FALSE END FROM Member m WHERE m.member_id = :member_id")
+	    boolean existsByMember_id(@Param("member_id") String member_id);
+	// email 중복 여부 확인
+	boolean existsByEmail(String email);
+
+	// name 중복 여부 확인
+	boolean existsByName(String name);
 
 }
