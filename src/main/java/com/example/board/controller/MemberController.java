813a9a8070a3243f721bd1898ff61f61c82f0700
Suna Choi <su00021zu@gmail.com>
2024-09-23 10:45:52 +0900

    memberPurchase

--- src/main/java/com/example/board/controller/MemberController.java
+++ src/main/java/com/example/board/controller/MemberController.java
@@ -18,6 +18,7 @@ import org.springframework.web.server.ResponseStatusException;
 import com.example.board.dto.MemberProfileDto;
 import com.example.board.dto.MemberUpdateRequest;
 import com.example.board.dto.PasswordUpdateRequest;
+import com.example.board.dto.PurchaseDTO;
 import com.example.board.model.member.LoginForm;
 import com.example.board.model.member.Member;
 import com.example.board.model.member.MemberJoinForm;
@@ -34,9 +35,6 @@ import java.io.FileInputStream;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
-
-
-
 @Slf4j
 @RestController
 @RequestMapping("/members")
@@ -44,23 +42,21 @@ public class MemberController {
 	private final MemberService memberService;
 	private final JwtTokenProvider jwtTokenProvider;
 
-	public MemberController(MemberService memberService, JwtTokenProvider jwtTokenProvider, MemberRepository memberRepository) {
+	public MemberController(MemberService memberService, JwtTokenProvider jwtTokenProvider,
+			MemberRepository memberRepository) {
 		this.memberService = memberService;
 		this.jwtTokenProvider = jwtTokenProvider;
 		this.memberRepository = memberRepository;
 
 	}
-	
+
 	@Autowired
 	private ProfileImageRepository profileImageRepository;
-    private final MemberRepository memberRepository;
-	
-	
+	private final MemberRepository memberRepository;
 
 	// 유저 등록
 	@PostMapping("/register")
 	public ResponseEntity<MemberJoinForm> registerMember(@Valid @RequestBody MemberJoinForm memberJoinForm) {
-
 
 		memberService.saveMember(memberJoinForm);
 		return ResponseEntity.ok(memberJoinForm);
@@ -98,81 +94,72 @@ public class MemberController {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
 		}
 	}
-	
+
 	// 프로필 이미지 업로드 API
 	@PostMapping("/profile/upload")
-	public ResponseEntity<Map<String, String>> uploadProfileImage(
-	        @RequestBody ProfileImageRequest profileImageRequest,
-	        @RequestHeader("Authorization") String token) {
-	    
-	    log.info("profileImageRequest: {}", profileImageRequest);
-	    
-	    try {
-	        // JWT 토큰에서 사용자 ID 추출
-	        String memberId = jwtTokenProvider.getUserIdFromToken(token.replace("Bearer ", ""));
-	        
-	        // 요청에서 받은 memberId와 비교 (선택 사항)
-	        if (!memberId.equals(profileImageRequest.getMemberId())) {
-	            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(Map.of("message", "권한이 없습니다."));
-	        }
+	public ResponseEntity<Map<String, String>> uploadProfileImage(@RequestBody ProfileImageRequest profileImageRequest,
+			@RequestHeader("Authorization") String token) {
 
-	        // 프로필 이미지 URL 처리 (예: 저장 또는 업데이트 로직)
-	        String imageUrl = profileImageRequest.getUrl();
-	        // 여기에 imageUrl을 파일로 저장하는 로직을 추가하세요
-	        
-	        memberService.saveProfileImage(memberId, imageUrl);
+		log.info("profileImageRequest: {}", profileImageRequest);
 
-	        Map<String, String> response = new HashMap<>();
-	        response.put("message", "프로필 이미지가 성공적으로 업로드되었습니다.");
-	        response.put("imageUrl", imageUrl); // 필요한 경우 이미지 URL 반환
+		try {
+			// JWT 토큰에서 사용자 ID 추출
+			String memberId = jwtTokenProvider.getUserIdFromToken(token.replace("Bearer ", ""));
 
-	        return ResponseEntity.ok(response);
-	    } catch (IllegalArgumentException e) {
-	        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("message", e.getMessage()));
-	    } catch (Exception e) {
-	        log.error("프로필 이미지 업로드 실패", e);
-	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("message", "업로드 중 오류 발생"));
-	    }
+			// 요청에서 받은 memberId와 비교 (선택 사항)
+			if (!memberId.equals(profileImageRequest.getMemberId())) {
+				return ResponseEntity.status(HttpStatus.FORBIDDEN).body(Map.of("message", "권한이 없습니다."));
+			}
+
+			// 프로필 이미지 URL 처리 (예: 저장 또는 업데이트 로직)
+			String imageUrl = profileImageRequest.getUrl();
+			// 여기에 imageUrl을 파일로 저장하는 로직을 추가하세요
+
+			memberService.saveProfileImage(memberId, imageUrl);
+
+			Map<String, String> response = new HashMap<>();
+			response.put("message", "프로필 이미지가 성공적으로 업로드되었습니다.");
+			response.put("imageUrl", imageUrl); // 필요한 경우 이미지 URL 반환
+
+			return ResponseEntity.ok(response);
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("message", e.getMessage()));
+		} catch (Exception e) {
+			log.error("프로필 이미지 업로드 실패", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("message", "업로드 중 오류 발생"));
+		}
 	}
 
-
-	
 	@GetMapping("/profile/images/{memberId}")
 	public ResponseEntity<Resource> getProfileImage(@PathVariable("memberId") String memberId) {
-	    try {
-	        // memberId로 Member 객체 조회
-	        Member member = memberRepository.findById(memberId)
-	                .orElseThrow(() -> new IllegalArgumentException("회원이 존재하지 않습니다."));
+		try {
+			// memberId로 Member 객체 조회
+			Member member = memberRepository.findById(memberId)
+					.orElseThrow(() -> new IllegalArgumentException("회원이 존재하지 않습니다."));
 
-	        // Member로 프로필 이미지 정보를 데이터베이스에서 조회
-	        ProfileImage profileImage = profileImageRepository.findByMember(member);
+			// Member로 프로필 이미지 정보를 데이터베이스에서 조회
+			ProfileImage profileImage = profileImageRepository.findByMember(member);
 
-	        if (profileImage == null) {
-	            return ResponseEntity.notFound().build(); // 프로필 이미지가 없으면 404 반환
-	        }
+			if (profileImage == null) {
+				return ResponseEntity.notFound().build(); // 프로필 이미지가 없으면 404 반환
+			}
 
-	        File file = new File(profileImage.getUrl());
-	        if (!file.exists()) {
-	            return ResponseEntity.notFound().build(); // 파일이 없으면 404 반환
-	        }
+			File file = new File(profileImage.getUrl());
+			if (!file.exists()) {
+				return ResponseEntity.notFound().build(); // 파일이 없으면 404 반환
+			}
 
-	        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
-	        return ResponseEntity.ok()
-	                .contentType(MediaType.IMAGE_JPEG) // 파일 형식에 맞게 수정
-	                .body(resource);
-	    } catch (IOException e) {
-	        log.error("이미지 로드 실패", e);
-	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-	    } catch (Exception e) {
-	        log.error("프로필 이미지 조회 실패", e);
-	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-	    }
+			InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+			return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG) // 파일 형식에 맞게 수정
+					.body(resource);
+		} catch (IOException e) {
+			log.error("이미지 로드 실패", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		} catch (Exception e) {
+			log.error("프로필 이미지 조회 실패", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
-
-
-
-
-
 
 	// 마이페이지
 	@GetMapping("/mypage")
@@ -242,38 +229,32 @@ public class MemberController {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
 		}
 	}
-	
-	//비밀번호 수정
 
-    @PutMapping("/password/update")
-    public ResponseEntity<Map<String, String>> updatePassword(
-            @RequestHeader("Authorization") String token,
-            @RequestBody PasswordUpdateRequest passwordUpdateRequest) {
+	// 비밀번호 수정
 
-        try {
-            // JWT 토큰에서 사용자 ID 추출
-            String memberId = jwtTokenProvider.getUserIdFromToken(token.replace("Bearer ", ""));
-            
-            // 비밀번호 변경 시도
-            boolean success = memberService.updatePassword(
-                    memberId,
-                    passwordUpdateRequest.getCurrentPassword(),
-                    passwordUpdateRequest.getNewPassword(),
-                    passwordUpdateRequest.getConfirmNewPassword()
-            );
-            
-            if (success) {
-                return ResponseEntity.ok(Map.of("message", "비밀번호가 성공적으로 변경되었습니다."));
-            } else {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body(Map.of("message", "비밀번호 변경에 실패했습니다. 현재 비밀번호 또는 새로운 비밀번호 확인을 해주세요."));
-            }
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(Map.of("message", "서버 오류로 비밀번호 변경에 실패했습니다."));
-        }
-    }
+	@PutMapping("/password/update")
+	public ResponseEntity<Map<String, String>> updatePassword(@RequestHeader("Authorization") String token,
+			@RequestBody PasswordUpdateRequest passwordUpdateRequest) {
 
+		try {
+			// JWT 토큰에서 사용자 ID 추출
+			String memberId = jwtTokenProvider.getUserIdFromToken(token.replace("Bearer ", ""));
+
+			// 비밀번호 변경 시도
+			boolean success = memberService.updatePassword(memberId, passwordUpdateRequest.getCurrentPassword(),
+					passwordUpdateRequest.getNewPassword(), passwordUpdateRequest.getConfirmNewPassword());
+
+			if (success) {
+				return ResponseEntity.ok(Map.of("message", "비밀번호가 성공적으로 변경되었습니다."));
+			} else {
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+						.body(Map.of("message", "비밀번호 변경에 실패했습니다. 현재 비밀번호 또는 새로운 비밀번호 확인을 해주세요."));
+			}
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body(Map.of("message", "서버 오류로 비밀번호 변경에 실패했습니다."));
+		}
+	}
 
 	// 판매 내역 조회
 	@GetMapping("mypage/sales/{member_id}")
@@ -303,8 +284,32 @@ public class MemberController {
 
 	}
 
+	// 구매 내역 조회
+	@GetMapping("mypage/purchases/{member_id}")
+	public ResponseEntity<List<PurchaseDTO>> getPurchaseHistory(@PathVariable("member_id") String memberId,
+			@RequestHeader("Authorization") String authorizationHeader) {
 
+		// Authorization 헤더에서 'Bearer' 접두어 제거
+		String token = authorizationHeader.startsWith("Bearer ") ? authorizationHeader.substring(7)
+				: authorizationHeader;
 
+		// 토큰 검증 및 멤버 ID 확인
+		if (!jwtTokenProvider.validateToken(token)) {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Invalid or expired token");
+		}
+
+		String tokenMemberId = jwtTokenProvider.getUserIdFromToken(token);
+
+		// 요청한 멤버 ID와 토큰에서 얻은 멤버 ID가 일치하는지 확인
+		if (!memberId.equals(tokenMemberId)) {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Access denied");
+		}
+
+		// 구매 내역 조회
+		List<PurchaseDTO> purchaseHistory = memberService.getPurchaseHistory(memberId);
+
+		return ResponseEntity.ok(purchaseHistory);
+	}
 
 	// 로그아웃 (JWT 기반에서는 특별한 로그아웃 처리가 필요하지 않음)
 	@PostMapping("/logout")
